ba062eff8368b6c90bc76eb5e1f9b610832a9607
Ning Sun <sunng@about.me>
2016-07-30 15:17:14 +0800

    (cargo-release) generate docs

---
+++ search-index.js
@@ -0,0 +1,3 @@
+var searchIndex = {};
+searchIndex["handlebars_template_handler"] = {"doc":"","items":[[3,"TemplateHandler","handlebars_template_handler","",null,null],[11,"new","","",0,{"inputs":[{"name":"str"}],"output":{"name":"templatehandler"}}],[11,"handle","","",0,null]],"paths":[[3,"TemplateHandler"]]};
+initSearch(searchIndex);
